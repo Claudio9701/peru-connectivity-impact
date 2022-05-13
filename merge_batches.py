6c960352c6b97b294947925cd9f291bebe46bcb9
Claudio Ortega <claudio.rtega2701@gmail.com>
2022-05-13 01:15:42 -0500

    remove unneceseray prints

--- merge_batches.py
+++ merge_batches.py
@@ -1,27 +1,17 @@
 import pandas as pd
 from pathlib import Path
 
-dfs = [pd.read_csv(fs, index_col=0) for fs in Path('outputs/').iterdir() if fs.name.startswith('geocoded_df') and fs.name.endswith('.csv')]
-
-# Concatenate and double check index integrity (i.e. no duplicates)
-geocoded_concatenate = pd.concat(dfs)
-
-print(geocoded_concatenate.index.duplicated().sum())
-print(geocoded_concatenate.shape)
-
+geocoded_df = pd.concat([pd.read_csv(fs, index_col=0) for fs in Path('outputs/').iterdir() if fs.name.startswith('geocoded_df') and fs.name.endswith('.csv')])
 df_main = pd.read_csv('outputs/df_not_geocoded_final.csv', index_col=0)
 
-print(df_main.head())
-print(df_main.shape)
+print('dfs shapes')
+print('gecocded_df:', geocoded_concatenate.shape)
+print('main_df:', df_main.shape)
 
 geocoded_df_orig_index = geocoded_concatenate.set_index(df_main.index)
-print(geocoded_df_orig_index.shape)
-print(geocoded_df_orig_index['lat'].isna().value_counts())
+print('Geocoded')
 print(geocoded_df_orig_index['lat'].notna().sum())
+print(round((geocoded_df_orig_index['lat'].notna().sum() / geocoded_df_orig_index.shape[0]) * 100, 2), '%')
 geocoded_df_orig_index_clean = geocoded_df_orig_index[geocoded_df_orig_index['lat'].notna().values]
-print(geocoded_df_orig_index_clean.shape)
+s
 geocoded_df_orig_index_clean.to_csv(f'outputs/geocoded_df_all_batches.csv')
-
-
-
-
